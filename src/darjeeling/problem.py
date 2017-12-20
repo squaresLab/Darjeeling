b86fc2cb7140585618030b92ea06c546d6f6f68f
ChrisTimperley <christimperley@googlemail.com>
2017-12-20 11:19:04 +0800

    added stub Problem class

---
+++ src/darjeeling/problem.py
@@ -0,0 +1,11 @@
+class Problem(object):
+    """
+    Used to provide a description of a problem (i.e., a bug), and to hold
+    information pertinent to its solution (e.g., coverage, transformations).
+    """
+    def __init__(self):
+        # program (named bug, Docker image, or given as source code)
+        # - transformation database
+        # - coverage
+        # - fault localisation?
+        pass
