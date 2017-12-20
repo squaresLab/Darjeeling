645b82f113503f122fac405725285b1f871ff007
ChrisTimperley <christimperley@googlemail.com>
2017-12-20 11:30:09 +0800

    elaborated Problem class

--- src/darjeeling/problem.py
+++ src/darjeeling/problem.py
@@ -1,11 +1,44 @@
+from bugzoo.bug import Bug
+
+
 class Problem(object):
     """
     Used to provide a description of a problem (i.e., a bug), and to hold
     information pertinent to its solution (e.g., coverage, transformations).
     """
-    def __init__(self):
-        # program (named bug, Docker image, or given as source code)
+    def __init__(self,
+                 bug: Bug,
+                 in_files: List[str] = None,
+                 in_functions: List[str] = None
+                 ) -> None:
+        """
+        Constructs a Darjeeling problem description.
+
+        Params:
+            bug: A description of the faulty program.
+            in_files: An optional list that can be used to restrict the set of
+                transformations to those that occur in any files belonging to
+                that list. If no list is provided, all source code files will
+                be included.
+            in_functions: An optional list that can be used to restrict the set
+                of transformations to those that occur in any function whose
+                name appears in the given list. If no list is provided, no
+                filtering of transformations based on the function to which
+                they belong will occur.
+        """
         # - transformation database
         # - coverage
         # - fault localisation?
-        pass
+
+        self.__in_files = in_files[:]
+        self.__in_functions = in_functions[:]
+
+        # program (named bug, Docker image, or given as source code)
+        self.__bug = None
+
+    @property
+    def bug(self) -> Bug:
+        """
+        A description of the bug, provided by BugZoo.
+        """
+        return self.__bug
