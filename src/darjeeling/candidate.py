6baa3a3a3deafb5771e7adacdb63819df696733d
ChrisTimperley <christimperley@googlemail.com>
2017-12-31 12:52:43 +0800

    working on patch generation

--- src/darjeeling/candidate.py
+++ src/darjeeling/candidate.py
@@ -1,4 +1,9 @@
 from typing import List, Iterator
+from darjeeling.transformation import Transformation, \
+                                      DeleteTransformation, \
+                                      ReplaceTransformation, \
+                                      AppendTransformation
+from darjeeling.problem import Problem
 
 
 class Candidate(object):
@@ -19,9 +24,41 @@ class Candidate(object):
         for t in self.__transformations:
             yield t
 
-    @property
-    def diff(self) -> str:
+    def diff(self, problem: Problem) -> str:
         """
-        Returns a plaintext diff for this repair.
+        Computes a plaintext diff for this repair.
         """
+        # group transformations by file
+        tf = {}
+        for t in self.transformations:
+            fn = t.line.filename
+            if fn not in tf:
+                tf[fn] = []
+            tf[fn].append(t)
+
+        # order each group of transformations in descending order of
+        # line number
+        for group in tf.values():
+            group.sort(key=lambda t: t.line.num, reverse=True)
+
+        # transform each group into a modified source code file
+        transformed = {}
+        for (fn, transformations) in tf.items():
+            src = problem.source(fn)
+            for t in transformations:
+                if isinstance(t, DeleteTransformation):
+                    src = src.with_line_removed(t.line.num)
+                elif isinstance(t, AppendTransformation):
+                    src = src.with_line_inserted(t.line.num + 1, t.snippet.content)
+                elif isinstance(t, ReplaceTransformation):
+                    src = src.with_line_replaced(t.line.num, t.snippet.content)
+                else:
+                    raise Exception("unsupported transformation")
+            transformed[fn] = src
+
+        # compute a diff for each modified source code file
+
+        # combine the diffs for each file into a single diff
+
         raise NotImplementedError
+        return diff
