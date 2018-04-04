8191dfa6c6126bd13319d9ef3825e15c26a06ba0
ChrisTimperley <christimperley@googlemail.com>
2018-04-04 16:03:22 -0400

    filter tweak

--- src/darjeeling/filters.py
+++ src/darjeeling/filters.py
@@ -5,7 +5,9 @@ def ends_with_semi_colon(contents: str) -> bool:
     """
     Returns `True` if a given line ends with a semi-colon.
     """
-    return contents.rstrip().endswith(';')
+    # FIXME add "is_statement"
+    contents = contents.strip()
+    return contents.endswith(';') and not contents == '};'
 
 
 def has_balanced_delimiters(contents: str) -> bool:
