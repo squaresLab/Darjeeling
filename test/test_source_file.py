ce63b2acc08444c6c92754c14a448bba8f070131
ChrisTimperley <christimperley@googlemail.com>
2018-04-13 07:06:37 -0400

    added another test case for source files

--- test/test_source_file.py
+++ test/test_source_file.py
@@ -17,7 +17,17 @@ Here's another line!
 
 
 def test_access():
-    char_range = FileCharRange(FileChar("test.txt", 12),
-                               FileChar("test.txt", 19))
+    char_range = FileCharRange(FileChar("test.txt", 11),
+                               FileChar("test.txt", 17))
     f_orig = SourceFile("test.txt", contents)
+    assert contents[11:18] == "example"
     assert f_orig[char_range] == "example"
+
+
+def test_delete():
+    char_range = FileCharRange(FileChar("test.txt", 11),
+                               FileChar("test.txt", 17))
+    f_orig = SourceFile("test.txt", contents)
+
+    f_mod = f_orig.delete(char_range)
+    assert str(f_mod) == contents[0:11] + contents[18:]
