6f2f356322a0485b68a8b330e13115aa1e4b5930
ChrisTimperley <christimperley@googlemail.com>
2018-06-06 12:30:27 -0400

    added Location to core module

--- src/darjeeling/core.py
+++ src/darjeeling/core.py
@@ -1,4 +1,4 @@
-__all__ = ['Replacement', 'FileLine', 'FileLocationRange']
+__all__ = ['Replacement', 'FileLine', 'FileLocationRange', 'Location']
 
 from boggart.core.replacement import Replacement
-from boggart.core.location import FileLocationRange, FileLine
+from boggart.core.location import FileLocationRange, FileLine, Location
