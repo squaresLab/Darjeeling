4955be1bf50c7e7f0a19d85d05b62b817b7133f3
ChrisTimperley <christimperley@googlemail.com>
2018-06-13 10:54:21 -0400

    updated dependency versions

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ setup(
     license='mit',
     python_requires='>=3.5',
     install_requires=[
-        'bugzoo>=2.1.7',
+        'bugzoo>=2.1.9',
         'rooibos>=0.3.0',
-        'boggart>=0.1.4',
+        'boggart>=0.1.6',
         'attrs>=17.2.0',
         'requests',
         'flask'
