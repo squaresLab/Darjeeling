b53a848f291e2b893eb31565750bb539e41aae3e
ChrisTimperley <christimperley@googlemail.com>
2018-07-08 13:11:41 -0400

    use bugzoo v2.1.14

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     license='mit',
     python_requires='>=3.5',
     install_requires=[
-        'bugzoo>=2.1.12',
+        'bugzoo>=2.1.14',
         'rooibos>=0.3.0',
         'boggart>=0.1.8',
         'kaskara>=0.0.1',
