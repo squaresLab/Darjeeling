8d2ef4deea41c41b2da685468aa0d71a3f8d2f2d
ChrisTimperley <christimperley@googlemail.com>
2018-10-08 20:51:27 -0400

    added --seed option

--- src/darjeeling/cli/__init__.py
+++ src/darjeeling/cli/__init__.py
@@ -1,6 +1,10 @@
 import logging
+import datetime
 
 import cement
+import yaml
+
+from ..exceptions import BadConfigurationException
 
 logger = logging.getLogger(__name__)  # type: logging.Logger
 logger.setLevel(logging.DEBUG)
@@ -25,12 +29,42 @@ class BaseController(cement.Controller):
         arguments=[
             (['filename'],
              {'help': ('a Darjeeling configuration file describing the faulty '
-                       'program and how it should be repaired.') })
+                       'program and how it should be repaired.') }),
+            (['--seed'],
+             {'help': 'random number generator seed',
+              'type': int})
         ]
     )
     def repair(self) -> None:
         filename = self.app.pargs.filename
-        print("hello!")
+        seed = self.app.pargs.seed
+
+        with open(filename, 'r') as f:
+            yml = yaml.load(f)
+
+        # seed override
+        if seed is not None:
+            logger.info("using random number generator seed override: %d",
+                        seed)
+
+        # no seed override; seed provided in config
+        elif 'seed' in yml:
+            if not isinstance(yml['seed'], int):
+                m = "'seed' property should be an int."
+                raise BadConfigurationFailure(m)
+            elif yml['seed'] < 0:
+                m = "'seed' property should be greater than or equal to zero."
+                raise BadConfigurationFailure(m)
+            seed = yml['seed']
+            logger.info("using random number generator seed provided by configuration: %d",  # noqa: pycodestyle
+                        seed)
+
+        # no seed override or provided in provided
+        # use current date/time
+        elif seed is None:
+            seed = int(datetime.now())
+            logger.info("using random number generator seed based on current date and time: %d",  # noqa: pycodestyle
+                        seed)
 
 
 class CLI(cement.App):
@@ -40,5 +74,9 @@ class CLI(cement.App):
 
 
 def main():
+    log_to_stdout = logging.StreamHandler()
+    log_to_stdout.setLevel(logging.DEBUG)
+    logger.addHandler(log_to_stdout)
+
     with CLI() as app:
         app.run()
