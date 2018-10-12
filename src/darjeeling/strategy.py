802849d04b6f5db4781c5a3536f4539baf9ab701
ChrisTimperley <christimperley@googlemail.com>
2018-10-11 20:34:15 -0400

    begun implementation of genprog strategy

---
+++ src/darjeeling/strategy.py
@@ -0,0 +1,69 @@
+from typing import Iterator, List
+import random
+
+from .candidate import Candidate
+from .transformation import Transformation
+
+Population = List[Individual]
+
+
+class Strategy(object):
+    pass
+
+
+
+
+@attr.ib(frozen=True)
+class GenProgStrategy(Strategy):
+    population_size = attr.ib(type=int)
+    num_generations = attr.ib(type=int)
+    rate_crossover = attr.ib(type=float)
+
+    def initial(self) -> Population:
+        """
+        Generates an initial population according to this strategy.
+        """
+        pop = Population([Candidate() for _ in self.population_size])
+        return self.mutate(pop)
+
+    def choose_transformation(self) -> Transformation:
+        raise NotImplementedError
+
+    def fitness(self, population: Population) -> Dict[Individual, float]:
+        """
+        Computes the fitness of each individual within a population.
+        """
+        raise NotImplementedError
+
+    def select(self, population: Population) -> Population:
+        # compute a fitness score for each individuala
+
+        # perform tournament selection
+        pass
+
+    def mutate(self, population: Population) -> Population:
+        offspring = Population()
+        for ind in offspring:
+            t = self.choose_transformation()
+            child = frozenset(ind.transformations | {t})
+            offspring.append(child)
+        return offspring
+
+    def crossover(self, pop: Population) -> Population:
+        offspring = Population()
+        pop = random.shuffle(pop)
+        size = len(pop)
+        for i in range(0, size, 2):
+            parents = pop[i:i+n]
+            offspring += parents
+            if len(parents) == 2 and random.random() <= self.rate_crossover:
+                child = self.one_point_crossover(px, py)
+                offspring.append(child)
+        return offspring
+
+    def run(self) -> Iterator[Candidate]:
+        pop = self.initial()
+        for g in range(0, self.num_generations + 1):
+            pop = self.crossover(pop)
+            pop = self.mutate(pop)
+            pop = self.select(pop)
