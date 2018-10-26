c1402a4d38523f5234a74fd07e0b4d1f28a114d0
ChrisTimperley <christimperley@googlemail.com>
2018-10-26 12:34:10 -0400

    added basic test_contains

--- tests/test_localization.py
+++ tests/test_localization.py
@@ -2,9 +2,25 @@ import pytest
 from bugzoo.core.filechar import FileCharRange, FileChar
 
 from darjeeling.localization import Localization
+from darjeeling.core import FileLine
 import darjeeling.exceptions
+
+l = FileLine.from_string
 
 
 def test_empty_throws_exception():
     with pytest.raises(darjeeling.exceptions.NoImplicatedLines):
         Localization({})
+
+
+def test_contains():
+    loc = Localization.from_dict({
+        'foo.c:1': 1.0,
+        'foo.c:2': 1.0,
+        'foo.c:3': 1.0
+    })
+    assert l('foo.c:1') in loc
+    assert l('foo.c:2') in loc
+    assert l('foo.c:3') in loc
+    assert l('foo.c:4') not in loc
+    assert l('bar.c:1') not in loc
