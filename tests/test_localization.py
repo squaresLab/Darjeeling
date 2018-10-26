ff4d8c0f8aed8b6952f3f979b617d1a77e733ecf
ChrisTimperley <christimperley@googlemail.com>
2018-10-26 13:20:59 -0400

    added test_get

--- tests/test_localization.py
+++ tests/test_localization.py
@@ -24,3 +24,16 @@ def test_contains():
     assert l('foo.c:3') in loc
     assert l('foo.c:4') not in loc
     assert l('bar.c:1') not in loc
+
+
+def test_get():
+    loc = Localization.from_dict({
+        'foo.c:1': 1.0,
+        'foo.c:2': 0.5,
+        'foo.c:3': 0.1
+    })
+    assert loc[l('foo.c:1')] == 1.0
+    assert loc[l('foo.c:2')] == 0.5
+    assert loc[l('foo.c:3')] == 0.1
+    assert loc[l('foo.c:4')] == 0.0
+    assert loc[l('bar.c:1')] == 0.0
