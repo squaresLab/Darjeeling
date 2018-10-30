7258aa192d052cf911c99dbed8be20fea6a548d5
ChrisTimperley <christimperley@googlemail.com>
2018-10-30 13:23:56 -0400

    added Language enum

--- src/darjeeling/core.py
+++ src/darjeeling/core.py
@@ -1,5 +1,13 @@
 __all__ = ['Replacement', 'FileLine', 'FileLocationRange', 'Location']
 
+from enum import Enum
+
 from boggart.core.replacement import Replacement
 from boggart.core.location import FileLocationRange, FileLine, Location, \
                                   LocationRange, FileLocation, FileLineSet
+
+
+class Language(Enum):
+    C = 'c'
+    CPP = 'cpp'
+    TEXT = 'text'
