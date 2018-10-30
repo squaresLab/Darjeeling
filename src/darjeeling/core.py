29bf8de7b473e50ffba58869ec4874b12de6951b
ChrisTimperley <christimperley@googlemail.com>
2018-10-30 13:32:35 -0400

    added find method to Language

--- src/darjeeling/core.py
+++ src/darjeeling/core.py
@@ -6,8 +6,17 @@ from boggart.core.replacement import Replacement
 from boggart.core.location import FileLocationRange, FileLine, Location, \
                                   LocationRange, FileLocation, FileLineSet
 
+from .exceptions import LanguageNotSupported
+
 
 class Language(Enum):
+    @classmethod
+    def find(cls, name: str) -> 'Language':
+        try:
+            return next(l for l in cls if l.value == name)
+        except StopIteration:
+            raise LanguageNotSupported(name)
+
     C = 'c'
     CPP = 'cpp'
     TEXT = 'text'
