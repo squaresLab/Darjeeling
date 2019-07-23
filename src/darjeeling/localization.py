1b9dc9d5a7ded03c8a351d2043a2aff94d4b6d46
ChrisTimperley <christimperley@googlemail.com>
2019-07-23 16:59:37 -0400

    ensure deterministic sampling

--- src/darjeeling/localization.py
+++ src/darjeeling/localization.py
@@ -173,19 +173,18 @@ class Localization:
             NoImplicatedLines: if no lines are determined to be suspicious.
             ValueError: if a line is assigned a negative suspiciousness.
         """
-        assert scores != []
         self.__line_to_score = scores.copy()
         self.__lines = []  # type: List[FileLine]
         self.__scores = []  # type: List[float]
-        for line, score in scores.items():
+        for line in sorted(scores):
+            score = scores[line]
             if score < 0.0:
                 raise ValueError("suspiciousness values must be non-negative.")
             if score == 0.0:
                 continue
             self.__lines.append(line)
             self.__scores.append(score)
-        self.__files = \
-            frozenset(line.filename for line in self.__lines)  # type: FrozenSet[str]  # noqa: pycodestyle
+        self.__files = [line.filename for line in self.__lines]
 
         if not self.__lines:
             raise NoImplicatedLines
@@ -316,16 +315,12 @@ class Localization:
         return self.__lines[i]
 
     def __len__(self) -> int:
-        """
-        Returns a count of the number of suspicious lines.
-        """
+        """Returns a count of the number of suspicious lines."""
         return len(self.__lines)
 
     @property
     def files(self) -> List[str]:
-        """
-        Returns a list of files that contain suspicious lines.
-        """
+        """A list of files that contain suspicious lines."""
         return list(self.__files)
 
     def __repr__(self) -> str:
