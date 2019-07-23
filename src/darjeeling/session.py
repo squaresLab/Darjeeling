3517705545cb1f9566b7b114e52c3a5553ede2a9
ChrisTimperley <christimperley@googlemail.com>
2019-07-23 19:34:43 -0400

    added stop method

--- src/darjeeling/session.py
+++ src/darjeeling/session.py
@@ -4,6 +4,10 @@ from .searcher import Searcher
 
 class Session:
     """Used to manage and inspect an interactive repair session."""
+    def stop() -> None:
+        """Stops the session."""
+        raise NotImplementedError
+
     def pause() -> None:
         """Pauses the session."""
         raise NotImplementedError
