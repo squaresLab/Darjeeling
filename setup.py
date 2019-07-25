b5a99e8a6027a204ecd6c30cbf2aa12d6847bab3
ChrisTimperley <christimperley@googlemail.com>
2019-07-25 16:34:17 -0400

    updated pyroglyph

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'boggart>=0.1.16',
         'kaskara>=0.0.5',
         'attrs>=17.4.0',
-        'pyroglyph~=0.0.3',
+        'pyroglyph~=0.0.4',
         'cement~=3.0.0',
         'requests',
         'flask'
