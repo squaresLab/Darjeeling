2b6c3b39be33a4f4627930982e930b2fdb2c03f9
ChrisTimperley <christimperley@googlemail.com>
2019-09-05 18:14:33 -0400

    added seed and terminate_early

--- src/config.py
+++ src/config.py
@@ -12,6 +12,11 @@ class Config:
 
     Attributes
     ----------
+    seed: int
+        The seed that should be used by the random number generator.
+    terminate_early: bool
+        Specifies whether or not the search should terminate upon
+        discovering an acceptable patch.
     num_threads: int
         The number of threads over which the search should be distributed.
     limit_candidates: int, optional
@@ -21,6 +26,8 @@ class Config:
         An optional limit on the number of minutes that may be spent
         searching for an acceptable patch.
     """
+    seed: int = attr.ib()  # FIXME use factory to generate default
+    terminate_early: bool = attr.ib(default=True)
     num_threads: int = attr.ib(default=1)
     limit_candidates: Optional[int] = attr.ib(default=None)
     limit_time_minutes: Optional[float] = attr.ib(default=None)
