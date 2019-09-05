b7838b85b244462b6d0b9f4fd98ce260b02e391c
ChrisTimperley <christimperley@googlemail.com>
2019-09-05 18:33:15 -0400

    added OptimizationsConfig

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__all__ = ('Config',)
+__all__ = ('Config', 'OptimizationsConfig')
 
 from typing import Optional
 
@@ -32,8 +32,22 @@ class Config:
     """
     snapshot: str = attr.ib()
     language: Language = attr.ib()
+    optimizations: OptimizationsConfig = attr.ib(factory=OptimizationsConfig)
     seed: int = attr.ib()  # FIXME use factory to generate default
     terminate_early: bool = attr.ib(default=True)
     num_threads: int = attr.ib(default=1)
     limit_candidates: Optional[int] = attr.ib(default=None)
     limit_time_minutes: Optional[float] = attr.ib(default=None)
+
+
+@attr.s
+class OptimizationsConfig:
+    """Specifies which optimizations should be applied during search."""
+    use_scope_checking: bool = attr.ib(default=False)
+    use_syntax_scope_checking: bool = attr.ib(default=True)
+    ignore_dead_code: bool = attr.ib(default=False)
+    ignore_equivalent_appends: bool = attr.ib(default=False)
+    ignore_untyped_returns: bool = attr.ib(default=False)
+    ignore_string_equivalent_snippets: bool = attr.ib(default=False)
+    ignore_decls: bool = attr.ib(default=True)
+    only_insert_executed_code = attr.ib(default=False)
