955e6fb54f3a179a1d417b3628a8b718f53496f0
ChrisTimperley <christimperley@googlemail.com>
2019-09-06 12:59:06 -0400

    added from_coverage and constructor to Spectra

--- src/darjeeling/spectra.py
+++ src/darjeeling/spectra.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 __all__ = ('Spectra', 'SpectraRow')
 
-from typing import Mapping, TypeVar
+from typing import Mapping, MutableMapping, Dict
 
 import attr
 
-T = TypeVar('T')
+from .core import TestCoverageMap, FileLine
 
 
 @attr.s(frozen=True, slots=True, auto_attribs=True)
@@ -32,12 +32,39 @@ class SpectraRow:
     nf: int
 
 
-class Spectra(Mapping[T, SpectraRow]):
-    def __len__(self) -> int:
-        return len(self.__contents)
+class Spectra:
+    """
+    A summary of the number of passing and failing tests covering each program
+    location.
+    """
+    @staticmethod
+    def from_coverage(cov: TestCoverageMap) -> 'Spectra':
+        num_fail = 0
+        num_pass = 0
+        tally_fail: MutableMapping[FileLine, int] = {}
+        tally_pass: MutableMapping[FileLine, int] = {}
 
-    def __getitem__(self, loc: T) -> SpectraRow:
-        return self.__contents[loc]
+        for test in coverage:
+            if test.outcome.successful:
+                tally = tally_pass
+                num_pass += 1
+            else:
+                tally = tally_fail
+                num_fail += 1
+            for line in coverage[test]:
+                tally[line] = tally.get(line, 0) + 1
 
-    def __iter__(self) -> Iterator[T]:
-        yield from self.__contents
+        return Spectra(num_pass, num_fail, tally_fail, tally_pass)
+
+    def __init__(self,
+                 num_passing: int,
+                 num_failing: int,
+                 tally_passing: Mapping[FileLine, int],
+                 tally_failing: Mapping[FileLine, int]
+                 ) -> None:
+        self.__num_passing = num_passing
+        self.__num_failing = num_failing
+        self.__tally_passing: Dict[str, Dict[int, int]] = \
+            FileLine.compactify(tally_passing)
+        self.__tally_failing: Dict[str, Dict[int, int]] = \
+            FileLine.compactify(tally_failing)
