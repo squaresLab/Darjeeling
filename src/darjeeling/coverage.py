a68b3d7763056e8f83123bc5347e4b2faf87e21a
ChrisTimperley <christimperley@googlemail.com>
2019-09-06 23:04:51 -0400

    provision bugzoo container

--- src/darjeeling/coverage.py
+++ src/darjeeling/coverage.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 __all__ = ('coverage_for_snapshot',)
 
-from bugzoo import Client as BugZooClient, Bug as Snapshot
+from bugzoo import (Client as BugZooClient,
+                    Bug as Snapshot,
+                    Container as BugZooContainer)
 
 from .core import TestCoverageMap, TestSuite, Test
 
@@ -10,4 +12,8 @@ def coverage_for_snapshot(bz: BugZooClient,
                           snapshot: Snapshot,
                           tests: TestSuite
                           ) -> TestCoverageMap:
-    raise NotImplementedError
+    container: BugZooContainer = bz.containers.provision(snapshot)
+    try:
+        raise NotImplementedError
+    finally:
+        del bz.containers[container.uid]
