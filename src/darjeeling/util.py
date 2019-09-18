4af82311fec591e67ec2c0b13ea6903c0fe97f9b
Chris Timperley <christimperley@googlemail.com>
2019-09-18 19:46:03 -0400

    Added reusable dynamically_registered decorator class (#163)
    * added DynamicallyRegistered class
    
    * bug fix: missing imports
    
    * added logging
    
    * updated SearchConfig to use DynamicallyRegistered
    
    * updated Searcher to use DynamicallyRegistered
    
    * tweaked type annotation
    
    * added __iter__ and __len__ methods
    
    * added _build_root method
    
    * added dynamic registration decorator
    
    * added dynamically_registered decorator
    
    * use decorator state to avoid polluting class
    
    * added argments to decorator
    
    * updated decorated classes
    
    * added type annotations

--- src/darjeeling/util.py
+++ src/darjeeling/util.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
 import warnings
-from typing import List, Union, Tuple
+import inspect
+import logging
+from typing import List, Union, Tuple, Any, Dict, Type, Set, Iterator, Optional
 from timeit import default_timer as timer
+
+logger = logging.getLogger(__name__)  # type: logging.Logger
+logger.setLevel(logging.DEBUG)
 
 
 def duration_tuple(secs: Union[int, float]) -> Tuple[int, int, int, int]:
@@ -40,6 +46,96 @@ def get_lines(fn: str) -> List[str]:
     Attempts to return a list of all the lines in a given source code file.
     """
     return [l.rstrip('\n') for l in get_file_contents(fn).splitlines()]
+
+
+def _dynamically_registered(cls,
+                            *,
+                            length: Optional[str] = '__len__',
+                            iterator: Optional[str] = '__iter__',
+                            lookup: Optional[str] = 'lookup',
+                            register_on: str = 'NAME'
+                            ):
+    logger.debug("Adding dynamic registration to class: %s", cls)
+    logger.debug("Registered via attribute: %s", register_on)
+
+    registry: Dict[str, Any] = {}
+    registered_class_names: Set[str] = set()
+
+    def method_hook_subclass(subcls, *args, **kwargs) -> None:
+        has_name = hasattr(subcls, register_on)
+        is_abstract = inspect.isabstract(subcls)
+        if has_name and is_abstract:
+            msg = f'Illegal "NAME" attribute in abstract class: {subcls}'
+            raise TypeError(msg)
+        if is_abstract:
+            return
+        if not has_name:
+            msg = f"Missing attribute '{register_on}' in class definition: {subcls}"
+            raise TypeError(msg)
+
+        # The use of class decorators may cause __init_subclass__ to be called
+        # several times for a given class. To avoid unexpected behaviour, we
+        # keep track of the names of the classes that have been registered, and
+        # we check whether a given class has been registered on the basis of
+        # its name. Note that we _must_ update the registration to point to the
+        # new class (since it's a different object).
+        name: str = getattr(subcls, register_on)
+        name_is_registered = name in registry
+        subcls_is_registered = subcls.__qualname__ in registered_class_names
+        if name_is_registered and not subcls_is_registered:
+            msg = f"Class already registered as '{name}': {subcls}"
+            raise TypeError(msg)
+
+        registry[name] = subcls
+
+        if subcls_is_registered:
+            logger.debug("updated [%s] registration for decorated class: %s",
+                         cls.__name__, subcls)
+        else:
+            logger.debug("added [%s] registration for class: %s",
+                         cls.__name__, subcls)
+            registered_class_names.add(subcls.__qualname__)
+
+    def method_length() -> int:
+        return len(registry)
+
+    def method_iterator() -> Iterator[str]:
+        yield from registry
+
+    def method_lookup(name: str):
+        return registry[name]
+
+    if length:
+        logger.debug("Adding length method [%s] to class [%s]", length, cls)
+        setattr(cls, length, staticmethod(method_length))
+    if iterator:
+        logger.debug("Adding iterator method [%s] to class [%s]",
+                     iterator, cls)
+        setattr(cls, iterator, staticmethod(method_iterator))
+    if lookup:
+        logger.debug("Adding lookup method [%s] to class [%s]", lookup, cls)
+        setattr(cls, lookup, staticmethod(method_lookup))
+
+    setattr(cls, '__init_subclass__', classmethod(method_hook_subclass))
+
+    logger.debug("Added dynamic registration to class: %s", cls)
+    return cls
+
+
+def dynamically_registered(register_on: str = 'NAME',
+                           *,
+                           length: Optional[str] = '__len__',
+                           iterator: Optional[str] = '__iter__',
+                           lookup: Optional[str] = 'lookup',
+                           ):
+    def decorator(cls):
+        return _dynamically_registered(cls,
+                                       register_on=register_on,
+                                       length=length,
+                                       iterator=iterator,
+                                       lookup=lookup)
+
+    return decorator
 
 
 class Stopwatch(object):
