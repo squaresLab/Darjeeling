eae69a83dc7646429a035ae04aecbfee68a1d6bc
Chris Timperley <christimperley@googlemail.com>
2019-09-22 14:00:28 -0400

    Added dynamic plugin via Flask-style naming conventions (#178)
    * added imports
    
    * added encoding header
    
    * added plugin loader
    
    * record loading of plugins
    
    * fixed plugin logging

--- src/darjeeling/__init__.py
+++ src/darjeeling/__init__.py
@@ -1,8 +1,23 @@
-import logging
+# -*- coding: utf-8 -*-
+import logging as _logging
+import importlib as _importlib
+import pkgutil as _pkgutil
 
 from . import exceptions
 from .version import __version__
 from .problem import Problem
 
-logging.getLogger(__name__).setLevel(logging.INFO)
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+_logger = _logging.getLogger(__name__)
+_logger.setLevel(_logging.INFO)
+_logger.addHandler(_logging.NullHandler())
+
+
+def _load_plugins() -> None:
+    """Dynamically loads all plugins for Darjeeling."""
+    for finder, name, is_pkg in _pkgutil.iter_modules():
+        if name.startswith('darjeeling_'):
+            _logger.info("loading plugin: %s", name)
+            _importlib.import_module(name)
+
+
+_load_plugins()
