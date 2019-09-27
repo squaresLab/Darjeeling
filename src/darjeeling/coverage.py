ac19184b5a563ae66c8b38f5c8e3b231aa4315cc
ChrisTimperley <christimperley@googlemail.com>
2019-09-27 10:18:39 -0400

    added extra logging to coverage

--- src/darjeeling/coverage.py
+++ src/darjeeling/coverage.py
@@ -3,6 +3,7 @@ __all__ = ('coverage_for_snapshot', 'coverage_for_container',
            'coverage_for_test')
 
 from typing import Set
+import logging
 import functools
 
 from bugzoo import (Client as BugZooClient,
@@ -12,11 +13,15 @@ from bugzoo import (Client as BugZooClient,
 from .core import FileLine, TestCoverageMap, Test, TestCoverage, TestOutcome
 from .test import TestSuite
 
+logger: logging.Logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 
 def coverage_for_snapshot(bz: BugZooClient,
                           snapshot: Snapshot,
                           tests: TestSuite
                           ) -> TestCoverageMap:
+    logger.debug("computing coverage for snapshot: %s", snapshot.name)
     container: BugZooContainer = bz.containers.provision(snapshot)
     try:
         return coverage_for_container(bz, container, tests)
@@ -28,7 +33,9 @@ def coverage_for_container(bz: BugZooClient,
                            container: BugZooContainer,
                            tests: TestSuite
                            ) -> TestCoverageMap:
+    logger.debug("instrumenting container for coverage...")
     bz.containers.instrument(container)
+    logger.debug("instrumented container for coverage")
     coverage = functools.partial(coverage_for_test, bz, container, tests)
     return TestCoverageMap({test.name: coverage(test) for test in tests})
 
@@ -38,6 +45,10 @@ def coverage_for_test(bz: BugZooClient,
                       tests: TestSuite,
                       test: Test
                       ) -> TestCoverage:
+    logger.debug("computing coverage for test [%s]", test.name)
     outcome: TestOutcome = tests.execute(container, test)
+    logger.debug("test outcome [%s]: %s", test.name, outcome)
+    logger.debug("extracting coverage for test [%s]", test.name)
     lines: Set[FileLine] = bz.containers.extract_coverage(container)
+    logger.debug("extracted coverage for test [%s]:\n%s", test.name, lines)
     return TestCoverage(test=test.name, outcome=outcome, lines=lines)
