e9b14e4d84f3677ef5203d2eb3ec6be0a735fd39
ChrisTimperley <christimperley@googlemail.com>
2019-10-15 11:20:19 -0400

    added on_build_started and on_build_finished

--- src/darjeeling/searcher/observer.py
+++ src/darjeeling/searcher/observer.py
@@ -3,7 +3,8 @@ __all__ = ('SearchObserver',)
 
 import abc
 
-from ..core import Test, TestOutcome
+from ..core import Test, TestOutcome, BuildOutcome
+from ..candidate import Candidate
 
 
 class SearchObserver(abc.ABC):
@@ -13,4 +14,15 @@ class SearchObserver(abc.ABC):
 
     @abc.abstractmethod
     def on_test_started(self, test: Test) -> None:
+        ...
+
+    @abc.abstractmethod
+    def on_build_started(self, candidate: Candidate) -> None:
+        ...
+
+    @abc.abstractmethod
+    def on_build_finished(self,
+                          candidate: Candidate,
+                          outcome: BuildOutcome
+                          ) -> None:
         ...
