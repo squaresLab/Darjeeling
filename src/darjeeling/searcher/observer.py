c8952f703f0b90b20c11946a9627d9073de18e44
ChrisTimperley <christimperley@googlemail.com>
2019-10-15 11:18:38 -0400

    added on_test_finished and on_test_started

--- src/darjeeling/searcher/observer.py
+++ src/darjeeling/searcher/observer.py
@@ -3,6 +3,14 @@ __all__ = ('SearchObserver',)
 
 import abc
 
+from ..core import Test, TestOutcome
+
 
 class SearchObserver(abc.ABC):
-    pass
+    @abc.abstractmethod
+    def on_test_finished(self, test: Test, outcome: TestOutcome) -> None:
+        ...
+
+    @abc.abstractmethod
+    def on_test_started(self, test: Test) -> None:
+        ...
