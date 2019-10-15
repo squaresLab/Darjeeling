fe0d3ea88d540057338b4e406197ac1ab1693e12
ChrisTimperley <christimperley@googlemail.com>
2019-10-15 12:31:09 -0400

    added docstrings

--- src/darjeeling/searcher/observer.py
+++ src/darjeeling/searcher/observer.py
@@ -9,16 +9,20 @@ from ..outcome import CandidateOutcome
 
 
 class SearchObserver(abc.ABC):
+    """Provides an interface for listening and responding to search events."""
     @abc.abstractmethod
     def on_test_finished(self, test: Test, outcome: TestOutcome) -> None:
+        """Called when a test execution has finished for a candidate patch."""
         ...
 
     @abc.abstractmethod
-    def on_test_started(self, test: Test) -> None:
+    def on_test_started(self, candidate: Candidate, test: Test) -> None:
+        """Called when a test execution has begun for a candidate patch."""
         ...
 
     @abc.abstractmethod
     def on_build_started(self, candidate: Candidate) -> None:
+        """Called when an attempt to build a candidate patch has begun."""
         ...
 
     @abc.abstractmethod
@@ -26,10 +30,12 @@ class SearchObserver(abc.ABC):
                           candidate: Candidate,
                           outcome: BuildOutcome
                           ) -> None:
+        """Called when an attempt to build a candidate patch has finished."""
         ...
 
     @abc.abstractmethod
     def on_candidate_started(self, candidate: Candidate) -> None:
+        """Called when the evaluation of a candidate patch has begun."""
         ...
 
     @abc.abstractmethod
@@ -37,4 +43,5 @@ class SearchObserver(abc.ABC):
                               candidate: Candidate,
                               outcome: CandidateOutcome
                               ) -> None:
+        """Called when the evaluation of a candidate patch has finished."""
         ...
