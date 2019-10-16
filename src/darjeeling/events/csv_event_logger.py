e39259f5d2d46fab08a95153fa97143151bfeac7
ChrisTimperley <christimperley@googlemail.com>
2019-10-16 19:52:30 -0400

    implemented _event_to_row

--- src/darjeeling/events/csv_event_logger.py
+++ src/darjeeling/events/csv_event_logger.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 __all__ = ('CsvEventLogger',)
 
-from typing import Sequence, TextIO
+from typing import Sequence, TextIO, Optional
 from typing_extensions import Protocol
 import os
 import csv
 
 import attr
 
-from .event import DarjeelingEvent
+from .event import (DarjeelingEvent,
+                    BuildStarted, BuildFinished,
+                    CandidateEvaluationStarted, CandidateEvaluationFinished,
+                    CandidateEvaluationError,
+                    TestExecutionStarted, TestExecutionFinished)
 from .handler import DarjeelingEventHandler
 
 
@@ -43,5 +47,38 @@ class CsvEventLogger(DarjeelingEventHandler):
         self._writer.writerow(row)
         self._file.flush()
 
+    def _event_to_row(self,
+                      event: DarjeelingEvent
+                      ) -> Optional[Sequence[str]]:
+        """Transforms an event to a CSV row."""
+        if isinstance(event, CandidateEvaluationStarted):
+            diff = "TODOTODOTODO"
+            return ['CANDIDATE-STARTED', event.candidate.id, diff]
+        if isinstance(event, CandidateEvaluationError):
+            diff = "TODOTODOTODO"
+            return ['CANDIDATE-ERROR', event.candidate.id, diff,
+                    str(event.error)]
+        if isinstance(event, CandidateEvaluationFinished):
+            diff = "TODOTODOTODO"
+            is_repair = event.outcome.is_repair
+            return ['PATCH-ACCEPTED' if is_repair else 'PATCH-REJECTED',
+                    event.candidate.id, diff]
+        if isinstance(event, BuildStarted):
+            return ['BUILD-STARTED', event.candidate.id]
+        if isinstance(event, BuildFinished):
+            status = 'passed' if event.outcome.successful else 'failed'
+            duration = f'{event.outcome.time_taken:.3f}'
+            return ['BUILD-OUTCOME', event.candidate.id, status, duration]
+        if isinstance(event, TestExecutionStarted):
+            return ['TEST-STARTED', event.candidate.id, event.test.name]
+        if isinstance(event, TestExecutionFinished):
+            status = 'passed' if event.outcome.successful else 'failed'
+            duration = f'{event.outcome.time_taken:.3f}'
+            return ['TEST-OUTCOME', event.candidate.id, event.test.name,
+                    status, duration]
+        return None
+
     def notify(self, event: DarjeelingEvent) -> None:
-        print(str(event))
+        row = self._event_to_row(event)
+        if row:
+            self._write(row)
