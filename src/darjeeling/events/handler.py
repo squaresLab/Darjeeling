8bea2e2f2e0f310922a23a2e31ecf302051b525e
ChrisTimperley <christimperley@googlemail.com>
2019-10-16 17:54:40 -0400

    added DarjeelingEventHandler

---
+++ src/darjeeling/events/handler.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__all__ = ('DarjeelingEventHandler',)
+
+import abc
+
+from .event import DarjeelingEvent
+
+
+class DarjeelingEventHandler(abc.ABC):
+    """Provides an interface for handling Darjeeling events."""
+    @abc.abstractmethod
+    def notify(self, event: DarjeelingEvent) -> None:
+        """Notifies this event handler of an event."""
+        ...
