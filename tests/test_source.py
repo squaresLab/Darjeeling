cd0de869fd6a7f58013c46ef347bccf93b787f66
ChrisTimperley <christimperley@googlemail.com>
2019-10-15 23:04:28 -0400

    added basic tests

---
+++ tests/test_source.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import os
+import tempfile
+
+import pytest
+
+from darjeeling.source import ProgramSource, ProgramSourceFile
+
+SIMPLE_FILE_NAME = 'simple.py'
+SIMPLE_FILE_CONTENTS = \
+"""
+@attr.s(frozen=True, slots=True)
+class TestOutcome:
+    \"\"\"Records the outcome of a test execution.\"\"\"
+    successful = attr.ib(type=bool)
+    time_taken = attr.ib(type=float)
+
+    @staticmethod
+    def from_bugzoo(outcome: BugZooTestOutcome) -> 'TestOutcome':
+        return TestOutcome(successful=outcome.passed,
+                           time_taken=outcome.duration)
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any]) -> 'TestOutcome':
+        return TestOutcome(d['successful'], d['time-taken'])
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {'successful': self.successful,
+                'time-taken': self.time_taken}
+""".strip()
+
+
+@pytest.fixture
+def simple_file():
+    return ProgramSourceFile(SIMPLE_FILE_NAME, SIMPLE_FILE_CONTENTS)
+
+
+def test_filename(simple_file):
+    assert simple_file.filename == SIMPLE_FILE_NAME
+
+
+def test_num_lines(simple_file):
+    assert simple_file.num_lines == 18
