63e2cb0e982724d37ffff0030a9ac1f46686f398
ChrisTimperley <christimperley@googlemail.com>
2020-01-22 06:39:40 +0800

    added import to __init__.py

--- src/darjeeling/__init__.py
+++ src/darjeeling/__init__.py
@@ -7,6 +7,7 @@ from loguru import logger as _logger
 _logger.disable('darjeeling')
 
 from . import exceptions
+from .container import ProgramContainer
 from .version import __version__
 from .environment import Environment
 from .problem import Problem
