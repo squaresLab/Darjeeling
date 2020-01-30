93402defb7bed2f169850f91fe97da35e99616d2
ChrisTimperley <christimperley@googlemail.com>
2020-01-30 09:12:46 -0500

    added AnnotatedTestExecution interface

---
+++ src/darjeeling/executor.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+__all__ = ('AnnotatedTestExecution',)
+
+import abc
+
+from .core import TestOutcome
+
+
+class AnnotatedTestExecution(abc.ABC):
+    """Provides additional information for a test execution in the form of an
+    annotation.
+    
+    Attributes
+    ----------
+    test_outcome: TestOutcome
+        The unannotated outcome of the test execution.
+    """
+    @property
+    @abc.abstractmethod
+    def test_outcome(self) -> TestOutcome:
+        ...
+
+    @property
+    def successful(self) -> bool:
+        """Indicates whether or not the test passed."""
+        return self.test_outcome.successful
+
+    @property
+    def time_taken(self) -> float:
+        """The number of seconds taken to execute the test."""
+        return self.test_outcome.time_taken
